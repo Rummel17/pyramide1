a55e32aebcd709b1332a2e40f4c1eafdc54ede4f
Rummel17 <p.rummel17@gmail.com>
2018-05-28 16:21:57 +0200

    final

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,23 +2,10 @@ $(document).ready(function() {
   //Add Block Functionality
   let values = [];
 
-
   $('#add-block .button').click(function() {
-
-    //create radom color
-    function randColor() {
-      var colors = ["green", "yellowgreen", "Chocolate", "goldenrod", "cadetblue", "firebrick", "magenta", "LightSeaGreen", "Peru", "Sienna", "SlateBlue", "Snow", "Tan", "Skyblue"];
-      var color = colors[Math.floor(Math.random() * colors.length)];
-
-      return color
-    }
-
-    //make color available gloabally
-    var resultColor = randColor();
 
     var blockNumber = $('#number-input').val();
     values.push(blockNumber);
-
 
     const sortedNumbers = values.sort(function (a, b){
       if (a > b) {
@@ -33,9 +20,10 @@ $(document).ready(function() {
     while (empty.firstChild) {
     empty.removeChild(empty.firstChild);
     }
+
     //This will do the trick, but I cannot include it in the pyramid correctly)
     // var test = document.querySelector('.pyramid').innerHTML = sortedNumbers.map(val => `<span class="values">${val}</span>`).join('');
-    values.forEach(function(element){
+    sortedNumbers.forEach(function(element){
 
       //determin widht of last div
       var lastwidth = $('.pyramid li:last-child .item').width();
@@ -47,11 +35,21 @@ $(document).ready(function() {
         var plus = lastwidth + 190; //not sure why 190 but with this value they line up smoothly. Was expecting 0 and 100 for the values.
       }
 
+      //create radom color
+      function randColor() {
+        var colors = ["green", "yellowgreen", "Chocolate", "goldenrod", "cadetblue", "firebrick", "magenta", "LightSeaGreen", "Peru", "Sienna", "SlateBlue", "Snow", "Tan", "Skyblue"];
+        var color = colors[Math.floor(Math.random() * colors.length)];
 
-    var $number = $('<li class="entry"><div class="item" style="width:' + plus + 'px; border-bottom: 60px solid ' + resultColor + ' ;"><span class="values">' + blockNumber + '</span></div></li>');
-    $('.pyramid').append($number);
-      })
-    // console.log(test);
+        return color
+      }
+
+      //make color available gloabally
+      var resultColor = randColor();
+
+      var $number = $('<li class="entry"><div class="item" style="width:' + plus + 'px; border-bottom: 60px solid ' + resultColor + ' ;"><span class="values">' + element + '</span></div></li>');
+      $('.pyramid').append($number);
+        })
+      // console.log(test);
 
 
 
